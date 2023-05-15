7580e61bc5ec42335d08cefde8e1405729ebb24e
Reid <49161354+reicaro@users.noreply.github.com>
2023-05-14 21:30:00 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -305,7 +305,7 @@ function App() {
               </div>
               <div className="flex-1 flex flex-col items-start justify-start gap-[8px] w-[200px]">
                 <div className="self-stretch relative tracking-[-0.1px] leading-[20px] font-medium">
-                  Speed
+                  Interval Length
                 </div>
                 <input
                   className="font-inter text-mini bg-base-white self-stretch rounded-md shadow-[0px_1px_2px_rgba(16,_24,_40,_0.04)] box-border h-[46px] overflow-hidden shrink-0 flex flex-row py-3 px-4 items-center justify-start border-[1px] border-solid border-neutral-700"
